03a68fd3345255337cf9498bdbe59c7bb08456ad
munquzbek <munchik31@gmail.com>
2023-10-09 05:19:21 -0400

    DB_utils creating func create_database for creating database and tables (companies, vanaccies)

--- database/DB_utils.py
+++ database/DB_utils.py
@@ -1,3 +1,41 @@
-def create_database():
-    pass
+import psycopg2
 
+
+def create_database(database_name: str, params: dict):
+    """Create database and tables about companies and vacancies."""
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True  # automatic commit after each sql query (lines: 10, 11)
+    cur = conn.cursor()
+
+    cur.execute(f'DROP DATABASE {database_name}')
+    cur.execute(f'CREATE DATABASE {database_name}')
+
+    cur.close()
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+    #   OPEN CURSOR FOR CREATING TABLE 'companies'
+    with conn.cursor() as cur:
+        cur.execute("""
+                    CREATE TABLE companies (
+                        company_id SERIAL PRIMARY KEY,
+                        company_name VARCHAR(255) NOT NULL,
+                        quantity_vacancies INTEGER,
+                        company_url TEXT
+                    )
+                """)
+    #   CREATING TABLE 'companies'
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancies (
+                vacancy_id SERIAL PRIMARY KEY,
+                company_id INT REFERENCES companies(company_id),
+                vacancy_name VARCHAR NOT NULL,
+                min_salary INTEGER,
+                max_salary INTEGER,
+                vacancy_url TEXT
+            )
+        """)
+
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from database.DB_utils import create_database
+from database.config import config
 from src.API import company_ids_from_api, get_info_each_company
 
 
 def main():
+    params = config()
+
     search_name = 'developer'
+
     company_ids = company_ids_from_api(search_name)
     data = get_info_each_company(company_ids)
-    print(data)
+    create_database('headhunter', params)
 
 
 if __name__ == '__main__':
