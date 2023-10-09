100b83d20eb683f1c2293a9f943e5de3c0fc1c7a
munquzbek <munchik31@gmail.com>
2023-10-09 04:03:35 -0400

    DEFINE NEEDED FUNCTIONS

---
+++ DBManager.py
@@ -0,0 +1,25 @@
+
+def get_companies_and_vacancies_count():
+    """Get list of all companies and quantity of vacancies of each company."""
+    pass
+
+
+def get_all_vacancies():
+    """Receives a list of all vacancies indicating the company name, vacancy title and salary,
+    and a link to the vacancy."""
+    pass
+
+
+def get_avg_salary():
+    """Get average salary of all vacancies."""
+    pass
+
+
+def get_vacancies_with_higher_salary():
+    """Get list of vacancies that salary higher than avg salary."""
+    pass
+
+
+def get_vacancies_with_keyword():
+    """Get list of vacancies with keyword. (ex: 'Python')"""
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from DBManager import get_companies_and_vacancies_count, get_all_vacancies, get_avg_salary, \
+    get_vacancies_with_higher_salary, get_vacancies_with_keyword
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
