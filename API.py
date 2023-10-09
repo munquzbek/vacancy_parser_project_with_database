325afae40ccc8461738d32b5bbf0751721c76f51
munquzbek <munchik31@gmail.com>
2023-10-09 04:21:20 -0400

    create get_info_each_company which return data about companies

--- API.py
+++ API.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+from typing import Any
+
+# URL OF API REQUEST
 api_url = 'https://api.hh.ru/vacancies'
 
 
@@ -20,3 +23,16 @@ def company_ids_from_api(search_name: str) -> list:
         print(vacancy['employer']['name'])
     return company_ids
 
+
+def get_info_each_company(company_ids: (list, Any)) -> list:
+    """Get info about each company, and vacancies of them"""
+    company_data = []
+    for company_id in company_ids:
+        params = {
+            'employer_id': company_id
+        }
+        response = requests.get(url=api_url, params=params)
+        data = response.content.decode()
+        vacancies = json.loads(data)
+        company_data.append(vacancies)
+    return company_data
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from API import company_ids_from_api
+from API import company_ids_from_api, get_info_each_company
 
 from DBManager import get_companies_and_vacancies_count, get_all_vacancies, get_avg_salary, \
     get_vacancies_with_higher_salary, get_vacancies_with_keyword
@@ -6,7 +6,8 @@ from DBManager import get_companies_and_vacancies_count, get_all_vacancies, get_
 
 def main():
     search_name = 'developer'
-    data = company_ids_from_api(search_name)
+    company_ids = company_ids_from_api(search_name)
+    data = get_info_each_company(company_ids)
     print(data)
 
 
